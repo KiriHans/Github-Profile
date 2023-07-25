9fff559fec803cdd182b921e1c9dc139d719a302
KiriHans <github.mxuxw@slmail.me>
2023-07-24 21:41:27 -0500

    New Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiGithubUrl: 'https://api.github.com',
-  apiKey: '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',
+  apiKey: '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',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiGithubUrl: 'https://api.github.com',
-  apiKey: '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',
+  apiKey: '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',
 };
 
 /*
